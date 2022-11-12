ca2385efde69fef89aa5b3491331a94d91e3faa9
YotamAmrani <87602958+YotamAmrani@users.noreply.github.com>
2022-11-12 17:05:08 +0200

    Update scraping_script.py
    updating due to readabilty issues

--- Scraping script/scraping_script.py
+++ Scraping script/scraping_script.py
@@ -10,7 +10,7 @@ import numpy as np
 from time import sleep
 import time
 
-# generate search options nodes:
+# generate search sets for nodes:
 
 _objects = {'table': 1, 'desk': 1, 'stool': 1, 'sofa': 1, 'chair': 1, 'shelf': 1, 'couch': 1,
             'nightstands': 1, 'drawer': 1, 'bench': 1, 'lamp': 1,
@@ -27,9 +27,11 @@ _shape = {
     'circle': 3, 'round': 3, 'ball': 3, 'sphere': 3, 'square': 3, 'triangle': 3, 'rectangle': 3,
     'box': 3, 'cube': 3, 'ring': 3, 'dots': 3, 'curve': 3, 'big': 3, 'small': 3, 'none': 3
 }
+
 _colors = {
     'red': 3, 'blue': 3, 'green': 3, 'yellow': 3, 'white': 3, 'black': 3, 'brown': 3, 'pink': 3, 'purple': 3,
     'orange': 3, 'grey': 3, 'none': 3}
+
 _general = {'illustration': 4, 'drawing': 4, 'sketch': 4, 'origami': 4, 'watercolor': 4,
             'vintage': 4, 'anime': 4, 'makeover': 4, 'diy': 4, 'modern': 4, 'design': 4, 'styling': 4, 'ideas': 4,
             'plans': 4, 'coffee': 4, 'garden': 4, 'office': 4, 'spaces': 4, 'bedroom': 4, 'wine': 4,
@@ -59,12 +61,13 @@ into JSON file.
 
 @params: list_of_search - list of nodes attributes.
 @return: None
-
 '''
 def build_vertix(list_of_search):
+    
+    # Creating a Data frame for pandas
     df = np.array([['id', 'search_tag', 'img_urls', 'neighbors']])
     df = pd.DataFrame(df)
-    counter = 1
+    counter = 1  # the vertex unique ID
 
     # open webdriver:
     driver = webdriver.Chrome('C:/ProgramData/chocolatey/lib/chromedriver/tools/chromedriver.exe')
@@ -73,27 +76,22 @@ def build_vertix(list_of_search):
     # Open the output file
     with open("Output_pins_log.txt", "w") as bu_file:
         for s in list_of_search:
-            # temp = s
-            # # case none is inside
-            # if 'none' in s:
-            #     temp = temp.discard('none')
-
+            
             current_vertix = []
-            # update id
+            # update The ID field on the DF
             current_vertix.append(counter)
 
-            # update string
-            # print(s)
             if s is not None:
+                # update The SEARCH TAGS field to the current vertix
                 current_vertix.append(list(s))
-                # update urls:
-                # prepare search
+                
+                # Extracting the Images url paths, by the current serch tags      
                 current_str = " ".join(s)
-                # call current search
-
-                driver.get(url + current_str)
+                driver.get(url + current_str) # call current search
                 times = 0
                 e = []
+                
+               # Pending for the searched elements - the images URL PATHS
                 while times < 4 and len(e) == 0:
                     e = driver.find_elements_by_class_name('GrowthUnauthPinImage')
                     sleep(0.1)
@@ -109,11 +107,13 @@ def build_vertix(list_of_search):
                         text += e[1].get_attribute('innerHTML')
                         text += e[2].get_attribute('innerHTML')
                         temp = scrap.parse_img_url(text)
-
+                    
+                    # update the current images url pathsto the current vertix
                     current_vertix.append(temp)
-                # add edges
+                # Set edges to be empty
                 current_vertix.append([])
-
+                
+                # Append the current vertix data to the result file
                 df.loc[counter] = current_vertix
                 counter += 1
                 bu_file.write(str(current_vertix) + '\n')
@@ -121,8 +121,8 @@ def build_vertix(list_of_search):
                 #     break
                 print(str(counter) + '/' + len(combo))
 
+     # Parsing result file to the required format     
     df.to_csv('Output_pins.csv', encoding='utf-8', index=False)
-
     df = df.to_json(orient='records')
     with open("Output_pins.json", "w") as text_file:
         # for p in df:
@@ -141,22 +141,3 @@ print('total: ' + str(end - start) + 'for ' + '100')
 
 
 
-
-
-# //script fo scraping data
-# // calling in a loop the scraping by the
-
-
-
-
-
-
-
-
-
-
-
-
-
-# soup = BeautifulSoup(r.text, 'html.parser')
-# result = soup.find_all('div', attrs= {'class':"Grid__Item"})
